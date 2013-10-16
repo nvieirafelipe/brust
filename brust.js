869df5614628c204ceee95124f568425c4f9b76b
Felipe Vieira <nvieirafelipe@gmail.com>
2013-10-16 13:07:51 -0300

    Fetch repositories by organization and show results on select tag

--- brust.js
+++ brust.js
@@ -1,19 +1,52 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to brust.";
+  Template.organization.organizationTitle = function() {
+    return "What's your organization ?";
+  };
+  Template.organization.organizationLabel = function () {
+    return "Organização: ";
   };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  Template.organization.repositories = function() {
+    return Session.get("repositories") || [];
+  }
+
+  Template.organization.events = {
+    'click #fetchButton' : function () {
+      $('#fetchButton').attr('disabled','true').val('Loading...');
+      organization = $('#organization').val();
+      Meteor.call('fetchRepositories', organization, function(err, repositories) {
+        if(err) {
+          window.alert("Error: " + err.reason);
+          console.log("error occured on receiving data on server. ", err );
+        } else {
+          Session.set("repositories", repositories);
+        }
+        $('#fetchButton').removeAttr('disabled').val('Fetch');
+      });
     }
-  });
+  };
+
+  document.title = Template.organization.organizationTitle();
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+  });
+
+  Meteor.methods({
+    fetchRepositories: function(organization) {
+      var url = "http://scorch-staging.herokuapp.com/organizations/"+organization+"/repositories";
+      var result = Meteor.http.get(url, {timeout:30000});
+      console.log(result)
+      if(result.statusCode==200) {
+        var respJson = JSON.parse(result.content);
+        return respJson['repositories'];
+      } else {
+        console.log("Response issue: ", result.statusCode);
+        var errorJson = JSON.parse(result.content);
+        throw new Meteor.Error(result.statusCode, errorJson.error);
+      }
+    }
   });
 }
